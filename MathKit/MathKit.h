c308fe8b6eb529936d082bf497cb988bcdc23fad
Rachel Brindle <you@subluminal.net>
2014-10-11 10:57:45 -0700

    Initial Commit

---
+++ MathKit/MathKit.h
@@ -0,0 +1,19 @@
+//
+//  MathKit.h
+//  MathKit
+//
+//  Created by Rachel Brindle on 10/11/14.
+//  Copyright (c) 2014 Rachel Brindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MathKit.
+FOUNDATION_EXPORT double MathKitVersionNumber;
+
+//! Project version string for MathKit.
+FOUNDATION_EXPORT const unsigned char MathKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathKit/PublicHeader.h>
+
+
