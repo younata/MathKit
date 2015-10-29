c119740992cd7931e40331b5e3c66b9cb93be6d8
Rachel Brindle <you@subluminal.net>
2015-10-29 12:30:04 -0700

    Make framework schemes shared

--- MathKit/Algebra.swift
+++ MathKit/Algebra.swift
@@ -1,47 +1,73 @@
-//import Foundation
-//
-//public struct Logarithm : Function {
-//    // first input is the base, second is the value.
-//    public var isOperator : Bool { return false }
-//    public var description: String { return "log" }
-//    
-//    public var numberOfInputs : Int { return 2 }
-//    
-//    public func apply(terms: [Double]) -> Double? {
-//        if terms.count < self.numberOfInputs {
-//            return nil
-//        }
-//        let c = terms.count
-//        return log(terms.last!) / log(terms[c-2])
-//    }
-//    
-//    public func differentiate(terms: [[PolynomialTerm]], respectTo: String) -> Polynomial? {
-//        return nil
-//    }
-//    
-//    public func integrate(terms: [[PolynomialTerm]], respectTo: String) -> Polynomial? {
-//        return nil
-//    }
-//}
-//
-//public struct NaturalLogarithm : Function {
-//    public var isOperator : Bool { return false }
-//    public var description: String { return "ln" }
-//    
-//    public var numberOfInputs : Int { return 1 }
-//    
-//    public func apply(terms: [Double]) -> Double? {
-//        if terms.count < self.numberOfInputs {
-//            return nil
-//        }
-//        return log(terms.last!)
-//    }
-//    
-//    public func differentiate(terms: [[PolynomialTerm]], respectTo: String) -> Polynomial? {
-//        return Polynomial(stack: [([PolynomialTerm(scalar: 1)], nil), (terms.first!, nil), (nil, Division())])
-//    }
-//    
-//    public func integrate(terms: [[PolynomialTerm]], respectTo: String) -> Polynomial? {
-//        return nil
-//    }
-//}
+import Foundation
+
+public struct Logarithm: Function, Equatable {
+    public var description: String {
+        return ""
+    }
+
+    public var latexDescription: String {
+        return self.description
+    }
+
+    public var numberOfInputs: Int { return 2 }
+    public private(set) var terms: [Polynomial]
+
+    public init(terms: [Polynomial]) {
+        self.terms = terms
+        assert(terms.count == self.numberOfInputs)
+    }
+
+    public func simplify() -> Polynomial? {
+        return nil
+    }
+
+    public func valueAt(x: [String : Double]) -> Double? {
+        return nil
+    }
+
+    public func differentiate(respectTo: String) -> Polynomial? {
+        return nil
+    }
+
+    public func integrate(respectTo: String) -> Polynomial? {
+        return nil
+    }
+}
+
+public func ==(a: Logarithm, b: Logarithm) -> Bool {
+    return  false
+}
+
+public struct NaturalLogarithm: Function {
+    public var description: String {
+        return ""
+    }
+
+    public var latexDescription: String {
+        return self.description
+    }
+
+    public var numberOfInputs: Int { return 1 }
+    public private(set) var terms: [Polynomial]
+
+    public init(terms: [Polynomial]) {
+        self.terms = terms
+        assert(terms.count == self.numberOfInputs)
+    }
+
+    public func simplify() -> Polynomial? {
+        return nil
+    }
+
+    public func valueAt(x: [String : Double]) -> Double? {
+        return nil
+    }
+
+    public func differentiate(respectTo: String) -> Polynomial? {
+        return nil
+    }
+
+    public func integrate(respectTo: String) -> Polynomial? {
+        return nil
+    }
+}
